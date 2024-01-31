bcda805a20744623baa216c62d6f844bd61536e3
Bibek <bistabibek9@gmail.com>
2024-01-31 22:26:25 +0545

    changes in mocks

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 
-const API_BASE_URL = "http://localhost:8001/api/v1";
+const API_BASE_URL = "https://localhost:8001/api/v1";
 
 export const handlers = [
   // User registration
